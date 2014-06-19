4d73772a8071e68b27fca5f2b33b7fc87896de67
Saif Abid <Saif282@gmail.com>
2014-06-19 01:21:31 -0400

    sum of numbers in file-complete

---
+++ easy/sumOfIntegersFromFile.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int toInt(int c){
+  int val=c-'0';
+  return val;
+}
+
+int main(int argc, char **argv){
+
+char * file_name=argv[1];
+FILE *f = fopen(file_name, "r");
+char num[100];
+int sum=0;
+int i=0;
+int num_array[100];
+
+while (fgets(num,100,f)!=NULL){
+
+sscanf(num,"%d",&num_array[i]);
+
+sum=sum+num_array[i];
+i++;
+
+}
+printf ("%d",sum);
+}
